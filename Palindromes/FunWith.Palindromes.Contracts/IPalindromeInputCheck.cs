42309460ade6b1545ac3cf09b9ae137473541f65
rsdev30 <rsdev30@gmail.com>
2021-08-29 16:24:43 -0500

    Next set of changes for palindromes and their unit tests.

---
+++ Palindromes/FunWith.Palindromes.Contracts/IPalindromeInputCheck.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunWith.Palindromes.Contracts
+{
+    public interface IPalindromeInputCheck<TInput>
+    {
+        bool IsPalindrome(TInput input);
+    }
+}
