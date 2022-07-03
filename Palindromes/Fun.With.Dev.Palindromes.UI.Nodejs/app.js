4d620be533c9e1e2bec0fbbfda003e1fafdadeb0
rsdev30 <rsdev30@gmail.com>
2022-07-03 16:40:23 -0500

    First changes for displaying known palindromes.

--- Palindromes/Fun.With.Dev.Palindromes.UI.Nodejs/app.js
+++ Palindromes/Fun.With.Dev.Palindromes.UI.Nodejs/app.js
@@ -1,8 +1,9 @@
 'use strict';
-var http = require('http');
+const http = require('http');
+const fs = require('fs')
 var port = process.env.PORT || 1337;
 
 http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Fun With Palindromes!\n');
+    res.writeHead(200, { 'Content-Type': 'text/html' });    
+    fs.createReadStream('index.html').pipe(res)
 }).listen(port);
