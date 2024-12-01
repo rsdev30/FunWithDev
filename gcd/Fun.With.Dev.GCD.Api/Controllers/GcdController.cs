03a52c03d8977ffbb96f0fab33a8df12fa8a0b2a
Ryan Schreck <schreck.ryan@gmail.com>
2024-12-01 09:56:41 -0600

    Very rough draft of the GCD algorithm.

--- gcd/Fun.With.Dev.GCD.Api/Controllers/GcdController.cs
+++ gcd/Fun.With.Dev.GCD.Api/Controllers/GcdController.cs
@@ -7,26 +7,73 @@ namespace WebApplication1.Controllers
     [ApiController]
     [Route("[controller]")]
     public class GcdController : ControllerBase
-    {         
-        private readonly ILogger<GcdController> _logger;
+    {
+        private readonly ILogger<GcdController>? _logger;
 
-        public GcdController(ILogger<GcdController> logger)
+        public GcdController(ILogger<GcdController>? logger)
         {
-            _logger = logger;
+            _logger = logger ?? throw new NullReferenceException(nameof(logger));
         }
 
+        /// <summary>
+        /// This very first implementation shows the code is coupled to the controller.
+        /// TODO: Refactor into a handler or non anemic domain model. 
+        /// </summary>
+        /// <param name="M"></param>
+        /// <param name="N"></param>
+        /// <returns></returns>
         [HttpGet(Name = "GetGcdResults")]
-        public GcdResults Get(uint M, uint N)
+        public async Task<ActionResult<GcdResults>> Get(uint M, uint N)
         {
-            var results = new GcdResults(); 
-            
-            //TODO: implementation goes here.
-            while (N > 0) 
+            return await Task.Run<ActionResult<GcdResults>>(() =>
             {
+                var result = new GcdResults();
 
-            }
+                try
+                {
 
-            return results;
+                    uint remainder = 0;
+                    result.M = M;
+                    result.N = N;
+                    result.Gcd = remainder;
+
+                    //Swap if M < N
+                    if (M < N)
+                    {
+                        uint tempM = M;
+                        uint tempN = N;
+
+                        M = tempN;
+                        N = tempM;
+
+                        result.M = M;
+                        result.N = N;
+                    }
+
+
+                    while (N > 0)
+                    {
+                        remainder = M % N;
+
+                        if (remainder == 0)
+                        {
+                            result.Gcd = N;
+                            break;
+                        }
+
+                        M = N;
+                        N = remainder;
+
+                    }
+
+                    return result;
+                }
+                catch (Exception ex)
+                {
+                    _logger?.LogCritical(ex, "Error calculating GCD.");
+                    return BadRequest("Error calculating GCD.");
+                }
+            });
         }
     }
 }
